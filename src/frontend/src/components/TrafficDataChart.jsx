729a28972fe2c4989cc88cff1a070df5000d1a3c
Klemen Kenda <klemen.kenda@ijs.si>
2020-03-23 13:32:15 +0100

    Graphs working

--- src/frontend/src/components/TrafficDataChart.jsx
+++ src/frontend/src/components/TrafficDataChart.jsx
@@ -6,6 +6,9 @@ import React, { Component } from 'react';
 // backend
 
 // import subcomponents
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 import './css/TrafficData.css';
 import { lZ } from '../lib/Utils';
 
@@ -18,34 +21,57 @@ type State = {};
 /**
  * Displaying production lines list.
  */
-class TrafficDataTable extends Component<Props, State> {
+class TrafficDataChart extends Component<Props, State> {
 
     render() {
 
         const data = this.props.data.reverse();
 
+        let dataArray = [];
+
+        for (const d of data) {
+            dataArray.push([new Date(d.ts * 1000).getTime(), d.value]);
+            console.log(new Date(d.ts * 1000));
+        }
+
+        const options = {
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: 'Pretok prometa'
+            },
+            xAxis: {
+                title: {
+                    text: null
+                },
+                type: 'datetime', //For time series, x-axis labels will be time
+                labels: {
+                    //You can format the label according to your need
+                    // format: '{value:%H:%m}'
+                },
+                // min: minDate,
+                // max: maxDate,
+                minPadding: 0.05,
+                maxPadding: 0.05
+            },
+            series: [
+                {
+                    name: "Pretork prometa",
+                    data: dataArray
+                }
+            ]
+        };
+
         return <div className="divTrafficData">
             <h3>Senzor {this.props.sensor_id}</h3>
             <a href="#no-data">Zapri</a>
-            <table>
-                <thead>
-                    <th>Čas</th>
-                    <th>Vrednost</th>
-                </thead>
-            {
-                data.map((el, i) => {
-                    const timestamp = new Date(parseInt(el.ts) * 1000);
-                    console.log(el);
-                    return <tr key={i}>
-                        <td>{lZ(timestamp.getHours()) + ":" + lZ(timestamp.getMinutes())}</td>
-                        <td>{el.value} avtov/h</td>
-                    </tr>
-                })
-            }
-            </table>
+            <div>
+                <HighchartsReact highcharts={Highcharts} options={options} />
+            </div>
         </div>
     }
 
 }
 
-export default TrafficDataTable;
+export default TrafficDataChart;
